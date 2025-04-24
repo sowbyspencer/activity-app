c344a742a3d268443695e12e4e5d4ea7f105cb88
sowbyspencer <sowbyspencer@gmail.com>
2025-04-24 10:16:53 -0600

    diff for repo update

--- api/config.ts
+++ api/config.ts
@@ -1,6 +1,6 @@
 // config.ts
 const LOCAL_DEV_IP = "10.34.69.46:5000";
 // const LOCAL_DEV_IP = "activity-app-server.onrender.com";
-
+// Diff for repo update
 
 export const API_URL = `http://${LOCAL_DEV_IP}`;

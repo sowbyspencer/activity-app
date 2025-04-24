b0fee50134352b02c3a4a3c4a8ce8758d84c391f
sowbyspencer <sowbyspencer@gmail.com>
2025-04-24 10:08:48 -0600

    config file

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,6 @@
 // config.ts
-const LOCAL_DEV_IP = "10.244.131.46:5000";
+const LOCAL_DEV_IP = "10.34.69.46:5000";
+// const LOCAL_DEV_IP = "activity-app-server.onrender.com";
 
-// export const API_URL = `http://${LOCAL_DEV_IP}`;
-export const API_URL = `https://activity-app-server.onrender.com`;
+
+export const API_URL = `http://${LOCAL_DEV_IP}`;
